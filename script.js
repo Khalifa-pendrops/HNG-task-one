141504808e65a76a101e9ee04d441b96caa4895d
khalifa-pendrops <dikee5200@gmail.com>
2024-07-01 19:13:05 +0100

    third commit

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const updated = new Date();
     const currentTimeUTC = updated.toUTCString().slice(17, 25);
     const currentDay = updated.toUTCString().slice(0, 3);
-    const currentYear = updated.getFullYear;
+    // const currentYear = updated.getFullYear;
 
 
     document.getElementById("current_time").textContent = currentTimeUTC;
     document.getElementById("current_day").textContent = currentDay;
-    document.getElementById("current_year").textContent = currentYear;
+    // document.getElementById("current_year").textContent = currentYear;
 })
